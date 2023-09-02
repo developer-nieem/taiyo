8e7d56b11539f39fb3c96c446794e3d26b0b42e4
developer-nieem <naimhossenwork@gmail.com>
2023-09-02 12:05:54 +0600

    added maps

--- src/components/ChartsAndMaps/ChartsAndMaps.tsx
+++ src/components/ChartsAndMaps/ChartsAndMaps.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PageTitle from '../Shared/PageTitle';
 
 import ChartsPage from './ChartsPage';
+import MapsPage from './MapsPage';
 
 const ChartsAndMaps = () => {
     return (
         <div>
             <PageTitle title='Charts and Maps page '></PageTitle>
             <ChartsPage></ChartsPage>
+            <MapsPage></MapsPage>
         </div>
     );
 };
---
+++ src/components/ChartsAndMaps/MapsPage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const MapsPage = () => {
+    const position: [number, number] = [51.505, -0.09];
+
+
+    return ( <>
+       <MapContainer style={{ width: '100%', height: '400px' }} center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>,
+      </>
+    );
+};
+
+export default MapsPage;
