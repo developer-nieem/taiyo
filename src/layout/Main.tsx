7b288ec3093af3882a6b75d364a69d07e261ab71
developer-nieem <naimhossenwork@gmail.com>
2023-09-02 13:51:45 +0600

    fix some kmown issues

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -7,7 +7,7 @@ const Main = () => {
   <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
   <div className="drawer-content flex flex-col  ">
         <Outlet></Outlet>
-    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open Sidebar</label>
   
   </div> 
   <div className="drawer-side">
